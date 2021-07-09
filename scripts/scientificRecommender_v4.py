e0d7dd0cf2524d46d8dcc083abd6a169019a0ac2
mandana-mazaheri <mndmazaheri@gmail.com>
2021-07-09 02:43:47 -0400

    Update Figures

--- scripts/scientificRecommender_v4.py
+++ scripts/scientificRecommender_v4.py
@@ -1,3 +1,7 @@
 from ipynb.fs.full.Recommender_v4 import scientificRecommender_v4
+from ipynb.fs.full.Recommender_v4 import pipelineExecutionFigure5_v4
+from ipynb.fs.full.Recommender_v4 import exprtConfidenceSwarmPlot_v4	
 
 scientificRecommender_v4()
+#pipelineExecutionFigure5_v4()
+#exprtConfidenceSwarmPlot_v4()   
