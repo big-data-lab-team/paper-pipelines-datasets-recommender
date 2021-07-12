18a6222c580941f62798da8e2818e7101156ee61
mandana-mazaheri <mndmazaheri@gmail.com>
2021-07-12 03:14:16 -0400

    Update Figures

--- scripts/scientificRecommender_v4.py
+++ scripts/scientificRecommender_v4.py
@@ -1,7 +1,9 @@
 from ipynb.fs.full.Recommender_v4 import scientificRecommender_v4
 from ipynb.fs.full.Recommender_v4 import pipelineExecutionFigure5_v4
 from ipynb.fs.full.Recommender_v4 import exprtConfidenceSwarmPlot_v4	
+from ipynb.fs.full.Recommender_v4 import getPipelinesAndDatasetsStatisctics_v4
 
-scientificRecommender_v4()
+#scientificRecommender_v4()
 #pipelineExecutionFigure5_v4()
 #exprtConfidenceSwarmPlot_v4()   
+getPipelinesAndDatasetsStatisctics_v4()
