f2f85030caf219a78ee9cae27c3c9425d4065785
mandana-mazaheri <mndmazaheri@gmail.com>
2021-07-14 00:13:20 -0400

    Update Figures

--- scripts/scientificRecommender_v4.py
+++ scripts/scientificRecommender_v4.py
@@ -3,9 +3,11 @@ from ipynb.fs.full.Recommender_v4 import pipelineExecutionFigure5_v4
 from ipynb.fs.full.Recommender_v4 import exprtConfidenceSwarmPlot_v4	
 from ipynb.fs.full.Recommender_v4 import getPipelinesAndDatasetsStatisctics_v4
 from ipynb.fs.full.Recommender_v4 import rocCurveEvaluation_v4
+from ipynb.fs.full.Recommender_v4 import successPercentageFigure_v4
 
 scientificRecommender_v4()
 #pipelineExecutionFigure5_v4()
 #exprtConfidenceSwarmPlot_v4()   
 #getPipelinesAndDatasetsStatisctics_v4()
 #rocCurveEvaluation_v4()
+#successPercentageFigure_v4()
